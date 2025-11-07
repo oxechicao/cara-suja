d96292245f1e44210aab13412a4048dde9d1e318
Chicão Thiago <fthiagogv@gmail.com>
2025-11-07 18:33:23 -0300

    feat: movida exceção customizada para camada de domínio

---
+++ src/main/java/ninja/oxente/cara_suja/domains/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package ninja.oxente.cara_suja.domains.exceptions;
+
+public class EntityNotFoundException extends Exception {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
